7f77ad575fe7c4ff608bbed0fd6e249ea311fce7
Hanish Jindal <hanish1139.cse19@chitkara.edu.in>
2021-12-23 16:54:32 +0530

    V1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login/Login";
+import { getTokenFromResponse } from "./components/Spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./components/Player/Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+  }, []);
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import { accessUrl } from "../Spotify";
 
 const Login = () => {
   return (
@@ -8,7 +9,9 @@ const Login = () => {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <button id="loginButton">LOG IN</button>
+      <a href={accessUrl} draggable="false" id="loginButton">
+        LOG IN
+      </a>
     </div>
   );
 };
